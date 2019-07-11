2dfbd441726c27a1d08b5221f0737266f07d9ebe
lukr <krausovalucia23@gmail.com>
2019-07-11 09:48:02 +0200

    initial commit

---
+++ src/main/java/guru/springframework/api/domain/UserData.java
@@ -0,0 +1,14 @@
+package guru.springframework.api.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class UserData {
+
+    List<User> data;
+
+}
